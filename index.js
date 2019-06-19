dce5e2029f7ca02558140d234c0c9c807a3a874f
taichi <ryushi@gmail.com>
2019-06-19 21:20:54 +0900

    more log

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ Toolkit.run(async tools => {
   tools.log('Welcome to this example!');
   tools.log(tools.getPackageJSON());
   tools.log(process.cwd());
+  tools.log(tools.context.repo);
+  tools.log(tools.context.ref);
+  tools.log(tools.context.event);
+
   const result = await tools.runInWorkspace('ls', ['-l']);
   tools.log(result);
   tools.exit.success('We did it!');
